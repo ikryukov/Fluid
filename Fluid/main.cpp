f50fd7a0f368fb9b487fc55347555185d97c0fb3
ikryukov <ilyakrukov@gmail.com>
2014-10-01 23:02:54 +0400

    Initial Commit

---
+++ Fluid/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fluid
+//
+//  Created by Ilya Kryukov on 01.10.14.
+//  Copyright (c) 2014 Ilya Kryukov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
